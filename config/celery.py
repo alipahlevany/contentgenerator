18d14fbe417a9ede4cda33d9f5242c430af52d13
alipahlevany <pahlevany.ali@gmail.com>
2026-07-13 12:21:50 +0330

    Route generation and delivery tasks to separate queues

--- config/celery.py
+++ config/celery.py
@@ -2,7 +2,10 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "config.settings",
+)
 
 app = Celery("config")
 
@@ -10,5 +13,20 @@ app.config_from_object(
     "django.conf:settings",
     namespace="CELERY",
 )
+
+app.conf.task_routes = {
+    "contents.tasks.run_generation_job_task": {
+        "queue": "generation",
+    },
+    "contents.tasks.run_daily_generation_task": {
+        "queue": "generation",
+    },
+    "contents.tasks.recover_stuck_generation_jobs": {
+        "queue": "generation",
+    },
+    "contents.tasks.send_model_data_to_api": {
+        "queue": "delivery",
+    },
+}
 
 app.autodiscover_tasks()
